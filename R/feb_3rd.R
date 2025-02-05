8344ba1ae8f83fc70a9716313acbce086a99f145
Matthew G. Son <gsist156@gmail.com>
2025-02-05 13:46:21 -0500

    Feb 5th update

---
+++ R/feb_3rd.R
@@ -0,0 +1,14 @@
+print("hello world!")
+
+print(700 + 1000)
+
+(3 > 1) & (3 <= 5)
+
+
+length(c(2,3) * c(3,9))
+
+user_name <- readline("What is your name?")
+
+
+c(98,99,100,101,102)
+bond_prices <-  c(98,99,100,101,102)
